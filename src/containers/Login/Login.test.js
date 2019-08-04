5814183be04b1f6de79edeef32f8520d5389ab9d
Izzati Alvandiar <al.vandiar@gmail.com>
2019-08-04 10:39:21 +0700

    rename test

--- src/containers/Login/Login.test.js
+++ src/containers/Login/Login.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Index from './index';
+import Login from './index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Index />, div);
+  ReactDOM.render(<Login/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
